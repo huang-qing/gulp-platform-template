c1456faf9f71793c794aa4629f4be01d8306c5f9
huang_qing <813293172@qq.com>
2017-03-15 10:08:09 +0800

    svg 测试

--- src/project/config.js
+++ src/project/config.js
@@ -1,17 +1,20 @@
-module.exports = function () {
+module.exports = function() {
     return {
         index: 2,
-        //对加载顺序有明确要求的CSS文件，按顺序写入配置项。
+        // 对加载顺序有明确要求的CSS文件，按顺序写入配置项。
         css: {
 
         },
-        //对加载顺序有明确要求的sass文件，按顺序写入配置项。
+        // 对加载顺序有明确要求的sass文件，按顺序写入配置项。
         sass: {
 
         },
-        //对加载顺序有明确要求的js文件，按顺序写入配置项。
+        // 对加载顺序有明确要求的js文件，按顺序写入配置项。
         js: {
 
+        },
+        svgSprite: {
+            'project-svg-sprite': 'images/SVG'
         }
     };
 };
